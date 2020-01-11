db767874581685a3746f3f95658fa9c41f81c011
zare926 <ilove.boc926@gmail.com>
2020-01-11 17:16:14 +0900

    非同期通信6

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,6 +10,7 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
+        format.html { redirect_to group_messages_path(@group), notice:'messeage complete'}
         format.json
       end
     else
