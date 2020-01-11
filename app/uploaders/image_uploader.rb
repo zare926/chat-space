456606ad59e4fe1242fe1f8dda1192b2bb783317
zare926 <ilove.boc926@gmail.com>
2020-01-11 15:44:40 +0900

    非同期通信5

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fit: [800, 800]
+  
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -45,4 +45,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  process resize_to_fit: [200, 200]
 end
+
