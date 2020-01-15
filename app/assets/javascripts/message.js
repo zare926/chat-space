abeb4a79d657380e0b401ba88f63205c17cde9c5
zare926 <ilove.boc926@gmail.com>
2020-01-15 19:01:57 +0900

    自動更新　修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -85,7 +85,7 @@ $(function(){
       }
     })
     .fail(function(){
-      console.log('error');
+      alert("メッセージ送信に失敗しました");
     });
   }; 
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
