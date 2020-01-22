72b77a3862940de8343119aaa244c6254c15e25d
zare926 <ilove.boc926@gmail.com>
2020-01-23 00:01:39 +0900

    unicorn設定

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -32,8 +32,6 @@ $(function(){
     let html= `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
     $(`#${userId}`).append(html);
 
-    // let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
-    // $(`#${userId}`).append(html);
   }
   $('#user-search-field').on("keyup", function(){
     let input = $('#user-search-field').val();
