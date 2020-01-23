381d1829a8262ce961e902f7f3d16e3e9806ab82
zare926 <ilove.boc926@gmail.com>
2020-01-23 19:48:37 +0900

    S3 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.11.2'
 
+set :linked_files, %w{ config/secrets.yml }
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
 
@@ -31,4 +32,16 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
